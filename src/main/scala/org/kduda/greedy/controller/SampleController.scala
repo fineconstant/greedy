e92400aae8ba46f84dc95d01ac348e7ba591658f
Kamil Duda <kamilduda01@gmail.com>
2017-01-01 18:49:43 +0100

    Add Thymeleaf hello world, needs fixes

--- src/main/scala/org/kduda/greedy/controller/SampleController.scala
+++ src/main/scala/org/kduda/greedy/controller/SampleController.scala
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.{GetMapping, RestController}
 @RestController
 class SampleController {
 
-  @GetMapping(Array("/"))
-  def defaultMapping = "scala default mapping"
+//  @GetMapping(Array("/"))
+//  def defaultMapping = "scala default mapping"
 
   @GetMapping(Array("/test"))
   def test(): Boolean = {
