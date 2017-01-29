8184df42e6fc4480e18be24c00250778b95bad76
Kamil Duda <kamilduda01@gmail.com>
2017-01-29 20:09:03 +0100

    Add parsing into .rul RSES file format

---
+++ src/main/scala/org/kduda/greedy/algorithm/parser/ToStringParser.scala
@@ -0,0 +1,34 @@
+package org.kduda.greedy.algorithm.parser
+
+object ToStringParser {
+
+  /**
+    * Creates String in a .rul RSES format ready to be saved in a file or DB.
+    *
+    * @param dtsRules Decision rules to be parsed.
+    * @return String containing rules in .rul RSES format.
+    */
+  def buildStringRSES(dtsRules: Map[String, List[List[(String, String)]]]): String = {
+    val builder = StringBuilder.newBuilder
+
+    for (dtRules <- dtsRules.values) {
+      for (rule <- dtRules) {
+        val support = rule.head._2
+        val decision = rule.tail.head
+        val conditions = rule.tail.tail
+
+        val formatted = conditions.map {
+          case (attr, value) =>
+            s"($attr={$value})"
+        }
+
+        builder.append(formatted.mkString("&"))
+
+        builder.append("=>")
+        builder.append(s"(${decision._1}={${decision._2}}[$support])")
+        builder.append(s" $support\n")
+      }
+    }
+    builder.mkString
+  }
+}
---
+++ src/test/java/org/kduda/greedy/unit/algorithm/parser/ToStringParserTest.java
@@ -0,0 +1,68 @@
+package org.kduda.greedy.unit.algorithm.parser;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kduda.greedy.algorithm.DecisionTableFactory;
+import org.kduda.greedy.algorithm.HeuristicsM;
+import org.kduda.greedy.algorithm.parser.ToStringParser;
+import org.kduda.greedy.spark.reader.csv.SparkCsvReader;
+import org.kduda.greedy.unit.SpringUnitTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import scala.Tuple2;
+import scala.collection.immutable.List;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ToStringParserTest extends SpringUnitTest {
+
+	@Autowired private SparkCsvReader sparkCsvReader;
+
+	private scala.collection.immutable.Map<String, Dataset<Row>> dtsMapped;
+
+	@SuppressWarnings("Duplicates")
+	@Before
+	public void setUp() throws IOException {
+		ClassPathResource resource = new ClassPathResource("/files/paper-sample.csv", getClass());
+		File file = new File(resource.getURL().getPath());
+
+		Map<String, String> options = new HashMap<>();
+		options.put("header", "true");
+
+		Dataset<Row> is = sparkCsvReader.read(file, options);
+		Dataset<Row>[] dtsInconsistent = DecisionTableFactory.extractDecisionTables(is);
+		Dataset<Row>[] consistent = DecisionTableFactory.removeInconsistenciesMCD(dtsInconsistent);
+		dtsMapped = DecisionTableFactory.createMapOf(consistent);
+	}
+
+	@After
+	public void tearDown() {
+		dtsMapped = null;
+	}
+
+	@Test
+	public void shouldBuildStringRSES() throws IOException {
+		scala.collection.immutable.Map<String, List<List<Tuple2<String, String>>>> rules = HeuristicsM.calculateDecisionRules(dtsMapped);
+
+		String actual = ToStringParser.buildStringRSES(rules);
+
+		ClassPathResource resource = new ClassPathResource("/files/paper-sample-rules.rul", getClass());
+		byte[] encoded = Files.readAllBytes(Paths.get(resource.getURI()));
+		String expected = new String(encoded, StandardCharsets.UTF_8);
+
+		assertThat(actual).isEqualTo(expected);
+	}
+}
