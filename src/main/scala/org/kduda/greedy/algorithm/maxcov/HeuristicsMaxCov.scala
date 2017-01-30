3fe1eee650c88f9822995c13e6a1b9fe6e50d087
Kamil Duda <kamilduda01@gmail.com>
2017-01-30 17:03:53 +0100

    Add maxCov tests

---
+++ src/main/scala/org/kduda/greedy/algorithm/maxcov/HeuristicsMaxCov.scala
@@ -0,0 +1,88 @@
+package org.kduda.greedy.algorithm.maxcov
+
+import org.apache.spark.sql.DataFrame
+import org.kduda.greedy.algorithm.DecisionRulesCalculator
+import org.kduda.greedy.algorithm.util.GreedyUtils
+import org.kduda.greedy.spark.generic.SparkAware
+
+import scala.collection.mutable.ArrayBuffer
+
+object HeuristicsMaxCov extends DecisionRulesCalculator with SparkAware {
+
+  // TODO: implement maxCov
+  override def calculateDecisionRules(dts: Map[String, DataFrame]): Map[String, List[List[(String, String)]]] = {
+    dts.map {
+      case (key, dt) =>
+        val result: ArrayBuffer[List[(String, String)]] = ArrayBuffer.empty
+        dt.cache()
+
+        val ALL_COLUMNS = dt.columns
+        val CONDITION_COLUMNS: ArrayBuffer[String] = ArrayBuffer(ALL_COLUMNS.dropRight(1): _*)
+        val DECISION_COLUMN = key
+
+        // if degenerated or empty return empty array
+        if (dt.select(DECISION_COLUMN).distinct().count() <= 1)
+          (key, List.empty[List[(String, String)]])
+        else {
+
+          var dtRows = dt.collect()
+
+          // calculating decision rule for each row
+          for (dtRow <- dtRows) {
+            val decision = dtRow.getAs[String](DECISION_COLUMN)
+            val conditionCols = CONDITION_COLUMNS.clone()
+
+            var distinctDecisions = 0
+            var decisionRule = ArrayBuffer((DECISION_COLUMN, dtRow.getAs[String](DECISION_COLUMN)))
+            do {
+              // potential columns with their M calculated, format: (M, column, value)
+              val colsWithM = ArrayBuffer.empty[(Float, String, String)]
+
+              // calculating M
+              for (col <- conditionCols) {
+                // N(T)
+                val NT = dtRows.filter(row => row.getAs[String](col) == dtRow.getAs[String](col))
+                val NTCount: Float = NT.length
+                // N(T, a)
+                val NTA = NT.filter(row => row.getAs[String](DECISION_COLUMN) == decision)
+                val NTACount: Float = NTA.length
+
+                // RM = (N(T), - N(T, a)) / N(T)
+                val RM = (NTCount - NTACount) / NTCount
+
+                colsWithM += Tuple3(RM, col, dtRow.getAs[String](col))
+              }
+              // order by the descending of value of M and get first item - it is the chosen column
+              val chosenCol = colsWithM.sortWith(_._1 < _._1).head
+
+              // row result (support, value) () <- () ^ ... () ^ ()
+              // head - support
+              // head - decision
+              // tail - condition
+              decisionRule += Tuple2(chosenCol._2, chosenCol._3)
+
+              // create separable subtable for created decision rule and check its decisions
+              val conditions = decisionRule.tail
+              val separableSubtable = GreedyUtils.filterByColumns(dtRows, conditions.toList)
+
+              // prepare for next iteration
+              distinctDecisions = GreedyUtils.countDistinctValuesIn(separableSubtable, DECISION_COLUMN)
+              if (distinctDecisions > 1) {
+                conditionCols -= chosenCol._2
+                // FIXME: if calculating with main decision table - remove next line
+                dtRows = GreedyUtils.filterByColumns(dtRows, List((chosenCol._2, chosenCol._3)))
+              }
+
+            }
+            while (distinctDecisions > 1)
+
+            val resultWithSupport = GreedyUtils.calculateSupport(dtRows, decisionRule)
+
+            result += resultWithSupport
+          }
+
+          (key, result.toList)
+        }
+    }
+  }
+}
---
+++ src/test/java/org/kduda/greedy/unit/algorithm/maxcov/HeuristicsMaxCovTest.java
@@ -0,0 +1,73 @@
+package org.kduda.greedy.unit.algorithm.maxcov;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kduda.greedy.algorithm.DecisionTableFactory;
+import org.kduda.greedy.algorithm.m.HeuristicsM;
+import org.kduda.greedy.algorithm.maxcov.HeuristicsMaxCov;
+import org.kduda.greedy.spark.reader.csv.SparkCsvReader;
+import org.kduda.greedy.unit.SpringUnitTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import scala.Tuple2;
+import scala.collection.immutable.List;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HeuristicsMaxCovTest extends SpringUnitTest {
+
+	@Autowired private SparkCsvReader sparkCsvReader;
+
+	private scala.collection.immutable.Map<String, Dataset<Row>> dtsMapped;
+
+	@SuppressWarnings("Duplicates")
+	@Before
+	public void setUp() throws IOException {
+		ClassPathResource resource = new ClassPathResource("/files/paper-sample.csv", getClass());
+		File file = new File(resource.getURL().getPath());
+
+		Map<String, String> options = new HashMap<>();
+		options.put("header", "true");
+
+		Dataset<Row> is = sparkCsvReader.read(file, options);
+		Dataset<Row>[] dtsInconsistent = DecisionTableFactory.extractDecisionTables(is);
+		Dataset<Row>[] consistent = DecisionTableFactory.removeInconsistenciesMCD(dtsInconsistent);
+		dtsMapped = DecisionTableFactory.createMapOf(consistent);
+	}
+
+	@After
+	public void tearDown() {
+		dtsMapped = null;
+	}
+
+	@Test
+	public void shouldReturnNonNullValue() {
+		scala.collection.immutable.Map<String, List<List<Tuple2<String, String>>>> result = HeuristicsMaxCov.calculateDecisionRules(dtsMapped);
+
+		assertThat(result).isNotNull();
+	}
+
+	@Test
+	public void shouldReturnMapWith3Elements() {
+		scala.collection.immutable.Map<String, List<List<Tuple2<String, String>>>> result = HeuristicsMaxCov.calculateDecisionRules(dtsMapped);
+
+		assertThat(result.size()).isEqualTo(3);
+	}
+
+	@Test
+	public void shouldReturnValidRules() {
+		scala.collection.immutable.Map<String, List<List<Tuple2<String, String>>>> result = HeuristicsMaxCov.calculateDecisionRules(dtsMapped);
+
+		assertThat(result.get("f3").get().size()).isEqualTo(0);
+		assertThat(result.get("f2").get().size()).isEqualTo(3);
+		assertThat(result.get("f1").get().size()).isEqualTo(2);
+	}
+}
