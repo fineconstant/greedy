22ef7eef5a201fa0ebca7c0d075dcc5c49e36a2f
Kamil Duda <kamilduda01@gmail.com>
2017-01-30 21:06:31 +0100

    Add blank line

--- src/test/java/org/kduda/greedy/unit/algorithm/poly/HeuristicsPolySpecialCasesTest.java
+++ src/test/java/org/kduda/greedy/unit/algorithm/poly/HeuristicsPolySpecialCasesTest.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HeuristicsPolySpecialCasesTest extends SpringUnitTest {
+
 	@Autowired private SparkCsvReader sparkCsvReader;
 
 	@Test
